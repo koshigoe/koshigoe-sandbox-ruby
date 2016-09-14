5fabaf2b66583b02ce9752a350f244acb5e32ee2
koshigoe <koshigoeb@gmail.com>
2016-09-14 11:48:58 +0900

    Add /image-base64

--- web.rb
+++ web.rb
@@ -1,4 +1,6 @@
 require 'sinatra'
+require 'base64'
+require 'json'
 
 get '/redirect-from-https-to-http.csv' do
   redirect request.url.gsub(/\Ahttps/, 'http') if request.secure?
@@ -7,4 +9,8 @@ a,b,c
 d,e,f
 g,h,i
 EOF
+end
+
+get '/image-base64' do
+  JSON.generate(body: Base64.strict_encode64(File.read('image-base64.png')))
 end
