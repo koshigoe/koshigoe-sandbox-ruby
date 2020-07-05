acdc17850f61f9ddc2a4a23e1957634e207df7cd
koshigoe <koshigoeb@gmail.com>
2020-07-06 08:46:41 +0900

    feat: add `GET /rss[?n=<int>]`

--- web.rb
+++ web.rb
@@ -181,3 +181,73 @@ post '/aws-transfer-auth' do
     }.to_json
   ]
 end
+
+get '/rss' do
+  n = (params[:n] || 10).to_i
+  pub_date = Time.now.getgm.strftime('%a, %d %b %Y %H:%M:%S GMT')
+
+  content_type 'application/rss+xml'
+  header = <<~XML
+    <?xml version="1.0" encoding="utf-8"?>
+    <rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:media="http://search.yahoo.com/mrss/" xmlns:snf="http://www.smartnews.be/snf">
+      <channel>
+        <title>スマートタイムス</title>
+        <link>http://times.smartnews.co.jp/</link>
+        <description>スマートなニュースをすべての人へ</description>
+        <pubDate>#{pub_date}</pubDate>
+        <language>ja</language>
+        <copyright>(c) SmartNews, Inc.</copyright>
+        <ttl>1</ttl>
+        <snf:logo><url>http://times.smartnews.co.jp/snlogo.png</url></snf:logo>
+  XML
+  items = n.times.map do |i|
+    <<~XML
+        <item>
+            <title>#{i}: 渋谷区桜丘エリアの再開発計画が決定</title>
+            <link>http://times.smartnews.co.jp/2014/06/16/sakuragaoka?#{i}</link>
+            <guid>http://times.smartnews.co.jp/2014/06/16/sakuragaoka?#{i}</guid>
+            <description><![CDATA[
+          渋谷駅桜丘口地区再開発準備組合と東急不動産株式会社は、2014年6月16日、渋谷区の市街地再開発等の都市開発計画が東京都により決定されたと発表した。
+            ]]></description>
+            <pubDate>#{pub_date}</pubDate>
+            <content:encoded><![CDATA[
+          <p>渋谷駅桜丘口地区再開発準備組合と東急不動産株式会社は、2014年6月16日、渋谷区の市街地再開発等の都市開発計画が東京都により決定されたと発表した。</p>
+          <p>この都市計画は、2013年12月19日より東京都知事に対して提案されていたもので、渋谷駅中心地区の都市基盤整備を完成させる重要なプロジェクトとなっている。</p>
+          <p>今後、周辺再開発と連携した縦動線アーバン・コア、歩行者デッキ、ネットワークの整備を行うほか、街区再編と併せた都市計画道路の整備および地下車路ネットワーク等の整備により、利便性・安全性の向上を図っていくとのこと。</p>
+          <figure>
+              <img src="img.png">
+              <figcaption>完成イメージ</figcaption>
+          </figure>
+            ]]></content:encoded>
+            <category>iphone,technology</category>
+            <dc:creator>須磨戸 太郎</dc:creator>
+            <dc:language>ja</dc:language>
+            <media:thumbnail url="http://times.smartnews.co.jp/2014/06/16/sakuragaoka/img.png" />
+          <snf:advertisement>
+            <snf:sponsoredLink link="http://times.smartnews.com/sponsored/article1.html" thumbnail="http://times.smartnews.com/sponsored/image1.jpg" title="桜丘に新スイーツ店が誕生！" advertiser="桜丘スイーツ"/>
+            <snf:sponsoredLink link="http://times.smartnews.com/sponsored/article2.html" thumbnail="http://times.smartnews.com/sponsored/image2.jpg" title="sponsored link 2" advertiser="Example Company"/>
+          </snf:advertisement>
+          <snf:analytics><![CDATA[
+            <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-xxx-2', 'examplecom');
+          ga('require', 'displayfeatures');
+          ga('set', 'referrer', 'http://www.smartnews.com/');
+          ga('send', 'pageview', '/260984/upsee/');
+            </script>
+            ]]>
+          </snf:analytics>
+        </item>
+    XML
+  end
+  footer = <<~XML
+    </channel>
+    </rss>
+  XML
+
+  header + items.join + footer
+end
